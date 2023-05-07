5f30d0ca85337731d73e76e540c12e6cc6e996bd
elainekedaibunga <132815632+elainekedaibunga@users.noreply.github.com>
2023-05-07 17:48:41 +0800

    test

---
+++ main.js
@@ -0,0 +1,39 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .flex .navbar');
+
+menu.onclick = () =>{
+   menu.classList.toggle('fa-times');
+   navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+   menu.classList.remove('fa-times');
+   navbar.classList.remove('active');
+}
+
+let filterBtn = document.querySelectorAll('.filter-buttons .buttons');
+let filterItem = document.querySelectorAll('.products .box-container .box');
+
+filterBtn.forEach(button =>{
+
+  button.onclick = () =>{
+    filterBtn.forEach(btn => btn.classList.remove('active'));
+    button.classList.add('active');
+
+    let dataFilter = button.getAttribute('data-filter');
+
+    filterItem.forEach(item =>{
+
+      item.classList.remove('active');
+      item.classList.add('hide');
+
+      if(item.getAttribute('data-item') == dataFilter || dataFilter == 'all'){
+        item.classList.remove('hide');
+        item.classList.add('active');
+      }
+
+    });
+
+  };
+
+});
